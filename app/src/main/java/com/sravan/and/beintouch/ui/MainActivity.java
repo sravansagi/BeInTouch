2272bf983b460c1d6ec25a5544f5337dd83087c6
Sravan <venkatasravan13@gmail.com>
2017-06-27 22:26:11 +0530

    Deleted menu settings from main activity
    Deleted menu settings from main activity

--- app/src/main/java/com/sravan/and/beintouch/ui/MainActivity.java
+++ app/src/main/java/com/sravan/and/beintouch/ui/MainActivity.java
@@ -167,28 +167,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         mAdView.loadAd(adRequest);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     /**
      * This method is part of the dextor library and is called when user allows a permission
      * @param permission the permission contains the name of permission that user allowed
